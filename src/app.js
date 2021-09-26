a9d18d707c2283887d7ccebbb6996dc1c8596b74
vallurirajesh@gmail.com <vallurirajesh@gmail.com>
2021-09-26 13:28:10 +1000

    changed portnumber to 3000

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const yoloRouter = require('../src/routers/yolo')
 const app = express()
 app.use(yoloRouter)
 
-const port = process.env.PORT || 3300
+const port = process.env.PORT || 3000
 console.log(port)
 app.listen(port, () => {
     console.log('Listening on port ' + port)
